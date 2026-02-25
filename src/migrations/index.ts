552619535e156c55713c14d4aa0addcf3be720d3
euhenrique <hfranca@mtrix.com.br>
2026-02-25 11:16:53 -0300

    Add new migration file for 20260225_141558 and update migrations index
    - Created a new migration file `20260225_141558.ts` with up and down functions.
    - Updated the migrations index to include the new migration.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,27 +1,9 @@
-import * as migration_20260219_182350_initial from './20260219_182350_initial';
-import * as migration_20260219_204024_add_meeting_with from './20260219_204024_add_meeting_with';
-import * as migration_20260224_215814 from './20260224_215814';
-import * as migration_20260225_134305 from './20260225_134305';
+import * as migration_20260225_141558 from './20260225_141558';
 
 export const migrations = [
   {
-    up: migration_20260219_182350_initial.up,
-    down: migration_20260219_182350_initial.down,
-    name: '20260219_182350_initial',
-  },
-  {
-    up: migration_20260219_204024_add_meeting_with.up,
-    down: migration_20260219_204024_add_meeting_with.down,
-    name: '20260219_204024_add_meeting_with',
-  },
-  {
-    up: migration_20260224_215814.up,
-    down: migration_20260224_215814.down,
-    name: '20260224_215814',
-  },
-  {
-    up: migration_20260225_134305.up,
-    down: migration_20260225_134305.down,
-    name: '20260225_134305'
+    up: migration_20260225_141558.up,
+    down: migration_20260225_141558.down,
+    name: '20260225_141558'
   },
 ];
